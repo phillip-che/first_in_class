4d2c7f7a4acb6f2ccb20ccd1acf44223b49c0d96
Phillip Che <pche@cpp.edu>
2021-09-30 00:02:47 -0700

    Added Outline for Guessing Game

---
+++ Number Guessing Game/guessingGame.c
@@ -0,0 +1,26 @@
+/*
+- initialize int variables for option, max value, user input, and the guess
+- initialize a character string which will take in user input
+- set max value to 10
+- print out menu with options
+- take in user input and assign value to character string
+- assign guess to a random variable between 1 and max value
+- if user enters option 1 prompt the user to enter their guess
+- game returns to menu if user enters 'q' and continues otherwise
+- parse the number inputted by the user and assign it to input variable
+- if input is equal to guess print out success message and return to menu
+- else if input is greater than guess then print out error message and allow user to guess again
+- else if input is lower than guess then print out error message and allow user to guess again
+- if user enters option 2 prompt user to enter a max value that is less than 10 but greater than 0
+- assign max value to the new input by the user or return error messagea if conditions are not met
+- if user enters option 3 end the game and print out exit message
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+
+}
