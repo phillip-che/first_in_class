c99b9ca81493fe27e613875003ce9cc8dafd9dd9
Phillip Che <pche@cpp.edu>
2021-09-29 20:17:02 -0700

    Added Outline

---
+++ Restaurant Bill/restaurantBill.c
@@ -0,0 +1,18 @@
+/*
+ - initialize tax percent, tax, tip percent, tip, item cost, and total bill variable 
+ - initialize and assign an array menu with size 4 to the cost of each menu item
+ - print menu items
+ - setup random variable to a number between 0-3
+ - assign cost variable to meal item with index of random variable
+ - assign tax to the tax percent divided by 100 multipled by the cost of the menu item
+ - assign tip to the tip percent divided by 100 multipled by the cost of the menu item
+ - assign total to the sum of cost, tax, and tip
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+}
