1e129397250bb64645bde9abe341176b3fed7d5d
Phillip Che <pche@cpp.edu>
2021-09-29 20:52:59 -0700

    Added Code Implementation

--- Restaurant Bill/restaurantBill.c
+++ Restaurant Bill/restaurantBill.c
@@ -15,4 +15,30 @@
 
 void main()
 {
+   double menu[4], taxPercent, tipPercent, tax, tip, cost, total;
+   time_t t;
+   menu[0] = 9.95;
+   menu[1] = 4.55;
+   menu[2] = 13.25;
+   menu[3] = 22.35;
+
+   printf("MENU\n1. Salad: %.2f\n2. Soup: %.2f\n3. Sandwich: %.2f\n4. Pizza: %.2f\n", 
+            menu[0], menu[1], menu[2], menu[3]);
+
+   srand((unsigned)time(&t));
+
+   printf("\nEnter Tax %: ");
+   scanf("%lf", &taxPercent);
+
+   printf("Enter Tip %: ");
+   scanf("%lf", &tipPercent);
+
+   int meal = (rand() % 4);
+
+   cost = menu[meal];
+   tax = (taxPercent / 100) * cost;
+   tip = (tipPercent / 100) * cost;
+   total = cost + tax + tip;
+
+   printf("\nBILL\nCost: $%.2f\nTax: $%.2f\nTip: $%.2f\nTotal: $%.2f", cost, tax, tip, total);   
 }
